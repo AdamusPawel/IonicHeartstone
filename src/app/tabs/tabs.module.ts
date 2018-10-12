135f4f1c39c0c848f1fc4f01318f58c2b889c90b
AdamusPawel <adamuspawel95@gmail.com>
2018-10-12 14:28:03 +0200

    Removed home page, added card page

--- src/app/tabs/tabs.module.ts
+++ src/app/tabs/tabs.module.ts
@@ -1,4 +1,4 @@
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, Card } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -9,7 +9,8 @@ import { TabsPageRoutingModule } from './tabs.router.module';
 import { TabsPage } from './tabs.page';
 import { ContactPageModule } from '../contact/contact.module';
 import { AboutPageModule } from '../about/about.module';
-import { HomePageModule } from '../home/home.module';
+
+import { CardPageModule } from '../card/card.module';
 
 @NgModule({
   imports: [
@@ -17,9 +18,9 @@ import { HomePageModule } from '../home/home.module';
     CommonModule,
     FormsModule,
     TabsPageRoutingModule,
-    HomePageModule,
     AboutPageModule,
-    ContactPageModule
+    ContactPageModule,
+    CardPageModule
   ],
   declarations: [TabsPage]
 })
