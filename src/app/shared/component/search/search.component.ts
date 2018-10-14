4f1c7de44f3c6c715b230757adcba6c49702aca5
AdamusPawel <adamuspawel95@gmail.com>
2018-10-14 15:01:44 +0200

    improved search with  debounceTime and distinctUntilChanged so requests aren't affecting performance of application that much

--- src/app/shared/component/search/search.component.ts
+++ src/app/shared/component/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, Output, EventEmitter } from "@angular/core";
+import { BehaviorSubject } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
     selector: 'app-search',
@@ -12,22 +14,23 @@ export class SearchComponent {
 
     @Output() searchCompleted = new EventEmitter();
 
+    private searchSubject = new BehaviorSubject<string>('');
+
     handleSearch(event: any) {
-        const searchedText = event.target.value;
 
-        if (!this.items) return this.searchCompleted.emit([]);
-        if (!searchedText) return this.searchCompleted.emit(this.items);
+        this.searchSubject.next(event.target.value);
+    }
 
-        const filteredItems = this.items.filter((item) => {
-            //return item.name.toLowerCase().includes(searchedText.toLowerCase());
+    ngAfterViewInit() {
+        this.searchSubject.pipe(debounceTime(500), distinctUntilChanged()).subscribe(searchedText => {
 
-            if (item[this.filteredProperty].toLowerCase().includes(searchedText.toLowerCase())) {
-                return true;
-            } else {
-                return false;
-            }
+            if (!this.items) return this.searchCompleted.emit([]);
+            if (!searchedText) return this.searchCompleted.emit(this.items);
+    
+            const filteredItems = this.items.filter((item) => {
+                return item.name.toLowerCase().includes(searchedText.toLowerCase());
+            });
+            this.searchCompleted.emit(filteredItems);
         })
-
-        this.searchCompleted.emit(filteredItems);
     }
 }
