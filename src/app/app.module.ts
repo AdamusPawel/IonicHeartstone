eb63c09897e572db2596684555a4f14b09f426b8
AdamusPawel <adamuspawel95@gmail.com>
2018-10-14 19:10:56 +0200

    added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,36 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { Firebase } from '@ionic-native/firebase/ngx';
+
+import { FcmService } from './shared/service/fcm.service';
+
+const config = {
+  apiKey: "AIzaSyAtFC_hreNbqxglEILZ_nUumcvXlBNnVEM",
+  authDomain: "heartstonelib-f1857.firebaseapp.com",
+  databaseURL: "https://heartstonelib-f1857.firebaseio.com",
+  projectId: "heartstonelib-f1857",
+  storageBucket: "heartstonelib-f1857.appspot.com",
+  messagingSenderId: "535660414890"
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot()],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    IonicStorageModule.forRoot(),
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule],
   providers: [
     StatusBar,
     SplashScreen,
+    Firebase,
+    FcmService,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
